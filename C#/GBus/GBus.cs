0f7fd6625716c574c3e7843ed7dfb3204c85a209
Timothy Garrett <powerkilroy@gmail.com>
2018-03-09 21:02:36 -0500

    Created GBus project

---
+++ C#/GBus/GBus.cs
@@ -0,0 +1,26 @@
+using System;
+/*      
+    What I need:
+    1. Number of TestCases
+    2. Number of GBuses
+    3. Tuple of (x,y) where x is the start and y is the end.
+    The start is the city starting point and the end is the 
+    city destination and it is inclusive so all numbers
+    between x and y count as well.
+    Each bus has a tuple to itself. Some buses go through the
+    same cities and this is allowed.
+    There are N buses so there should be n tuples.
+    The next is the number of cities we want to track,
+    followed by the number of that city.
+
+    GOAL: Find how many buses run through the particular cities
+    we are tracking.
+
+    OUTPUT: "Case #Ti:" where i is the testcase.
+*/
+class GBus
+{
+    static void Main(string[] args){
+        
+    }
+}
