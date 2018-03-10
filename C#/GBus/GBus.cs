c9fd3edef3d58125df3d033f295e50b232275a40
Timothy Garrett <powerkilroy@gmail.com>
2018-03-10 10:59:36 -0500

    Got Tuples working in a list. Only Functional not actual use yet.

--- C#/GBus/GBus.cs
+++ C#/GBus/GBus.cs
@@ -1,10 +1,12 @@
 using System;
 using System.IO;
+using System.Collections.Generic;
 /*      
     What I need:
-    1. Number of TestCases
-    2. Number of GBuses
-    3. Tuple of (x,y) where x is the start and y is the end.
+    1. Tuple of (x,y) where x is the start and y is the end.
+    2. Need to build out functions for getting the data parsed
+    from the file.
+
     The start is the city starting point and the end is the 
     city destination and it is inclusive so all numbers
     between x and y count as well.
@@ -22,19 +24,17 @@ using System.IO;
 class GBus
 {
     static void Main(string[] args){
-        
-        /*int testCases = Int32.Parse(args[0]);
-        int gBuses = Int32.Parse(args[1]);
-        int count = 2;
-        int numOfCities;
-        int[] cities = new int[gBuses*2];
-        for(int i = 0; i < gBuses * 2; i++){
-            cities[i] = Int32.Parse(args[count]);
-            count++;
-        }
-        numOfCities = Int32.Parse(args[count+1]);*/
 
-        TextReader reader = File.OpenText("PLACE TEXT FILE LOCATION HERE");
+        List<Tuple<int, int>> test = new List<Tuple<int,int>>();
+        test.Add(new Tuple<int, int>(4,2));
+        test.Insert(1, new Tuple<int,int>(1,2));
+        Tuple<int, int> tempTuple;
+        tempTuple = test[1];
+        int addition = tempTuple.Item1 + tempTuple.Item2;
+        Console.WriteLine(addition);
+        
+
+        /*TextReader reader = File.OpenText("PLACE TEXT FILE LOCATION HERE");
         string text = reader.ReadLine();
         
         int testCases = Int32.Parse(text);
@@ -53,11 +53,11 @@ class GBus
         int numOfCities;
         text = reader.ReadLine();
         numOfCities = Int32.Parse(text);
-        
+
         int[] citiesToVisit = new int[numOfCities];
         for(int i = 0; i < numOfCities; i++){
             text = reader.ReadLine();
             citiesToVisit[i] = Int32.Parse(text);
-        }
+        }*/
     }
 }
