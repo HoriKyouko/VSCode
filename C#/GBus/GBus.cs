4381106dbb5c9ffef90adb2b6c83e99353881b4e
Timothy Garrett <powerkilroy@gmail.com>
2018-03-10 08:37:41 -0500

    Created the beginning input for the file. Needs to be cleaned up and rewritten.

--- C#/GBus/GBus.cs
+++ C#/GBus/GBus.cs
@@ -1,4 +1,5 @@
 using System;
+using System.IO;
 /*      
     What I need:
     1. Number of TestCases
@@ -22,5 +23,41 @@ class GBus
 {
     static void Main(string[] args){
         
+        /*int testCases = Int32.Parse(args[0]);
+        int gBuses = Int32.Parse(args[1]);
+        int count = 2;
+        int numOfCities;
+        int[] cities = new int[gBuses*2];
+        for(int i = 0; i < gBuses * 2; i++){
+            cities[i] = Int32.Parse(args[count]);
+            count++;
+        }
+        numOfCities = Int32.Parse(args[count+1]);*/
+
+        TextReader reader = File.OpenText("PLACE TEXT FILE LOCATION HERE");
+        string text = reader.ReadLine();
+        
+        int testCases = Int32.Parse(text);
+        text = reader.ReadLine();
+
+        int gBuses = Int32.Parse(text);
+        text = reader.ReadLine();
+        
+        string[] bits = text.Split(' ');
+        
+        int[] cities = new int[gBuses * 2];
+        
+        for(int i = 0; i < cities.Length; i++){
+            cities[i] = Int32.Parse(bits[i]);
+        }
+        int numOfCities;
+        text = reader.ReadLine();
+        numOfCities = Int32.Parse(text);
+        
+        int[] citiesToVisit = new int[numOfCities];
+        for(int i = 0; i < numOfCities; i++){
+            text = reader.ReadLine();
+            citiesToVisit[i] = Int32.Parse(text);
+        }
     }
 }
