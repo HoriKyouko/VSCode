841ec111bc306bd21d6453c4122ae7b47367e2c9
Timothy Garrett <powerkilroy@gmail.com>
2018-03-11 10:29:34 -0400

    Implemented input of text files. Got Tuples working. Generic parse

--- C#/GBus/GBus.cs
+++ C#/GBus/GBus.cs
@@ -23,41 +23,64 @@ using System.Collections.Generic;
 */
 class GBus
 {
-    static void Main(string[] args){
-
-        List<Tuple<int, int>> test = new List<Tuple<int,int>>();
-        test.Add(new Tuple<int, int>(4,2));
-        test.Insert(1, new Tuple<int,int>(1,2));
+    static void Main(string[] args)
+    {
+        /*List<Tuple<int, int>> test = new List<Tuple<int, int>>();
+        test.Add(new Tuple<int, int>(4, 2));
+        test.Insert(1, new Tuple<int, int>(1, 2));
         Tuple<int, int> tempTuple;
         tempTuple = test[1];
         int addition = tempTuple.Item1 + tempTuple.Item2;
-        Console.WriteLine(addition);
-        
-
-        /*TextReader reader = File.OpenText("PLACE TEXT FILE LOCATION HERE");
-        string text = reader.ReadLine();
-        
-        int testCases = Int32.Parse(text);
-        text = reader.ReadLine();
-
-        int gBuses = Int32.Parse(text);
-        text = reader.ReadLine();
-        
-        string[] bits = text.Split(' ');
-        
-        int[] cities = new int[gBuses * 2];
-        
-        for(int i = 0; i < cities.Length; i++){
-            cities[i] = Int32.Parse(bits[i]);
-        }
+        Console.WriteLine(addition);*/
+        int testCases;
+        int gBuses;
+        List<Tuple<int,int>> tuples;
         int numOfCities;
+        int[] citiesToVisit;
+
+        TextReader reader = File.OpenText(path: "GBus.txt");
+        string text = reader.ReadLine();
+
+        testCases = parsedInt(text);
+
+        Tuple<int, List<Tuple<int,int>>,int, int[]> tuple = dataFromFile(reader);
+        gBuses = tuple.Item1;
+        tuples = tuple.Item2;
+        numOfCities = tuple.Item3;
+        citiesToVisit = tuple.Item4;
+        Console.WriteLine(testCases);
+        Console.WriteLine(gBuses);
+        Console.WriteLine(tuples.Count);
+        Console.WriteLine(numOfCities);
+        Console.WriteLine(citiesToVisit.Length);
+    }
+
+    private static Tuple<int, List<Tuple<int, int>>, int, int[]> dataFromFile(TextReader reader)
+    {
+        string text = reader.ReadLine();
+        int gBuses = parsedInt(text);
+
         text = reader.ReadLine();
-        numOfCities = Int32.Parse(text);
+        string[] bits = text.Split(' ');
+        List<Tuple<int, int>> tuples = new List<Tuple<int, int>>();
+        
+        for (int i = 0; i < gBuses*2; i+=2)
+        {
+            tuples.Add(new Tuple<int,int>(parsedInt(bits[i]), parsedInt(bits[i+1])));
+        }
+
+        text = reader.ReadLine();
+        int numOfCities = parsedInt(text);
 
         int[] citiesToVisit = new int[numOfCities];
-        for(int i = 0; i < numOfCities; i++){
+        for (int i = 0; i < numOfCities; i++)
+        {
             text = reader.ReadLine();
-            citiesToVisit[i] = Int32.Parse(text);
-        }*/
+            citiesToVisit[i] = parsedInt(text);
+        }
+        Tuple<int, List<Tuple<int, int>>, int, int[]> tempTuple = Tuple.Create(gBuses, tuples, numOfCities, citiesToVisit);
+        return tempTuple;
     }
+
+    public static int parsedInt(string text) => Int32.Parse(text);
 }
